27ec10f6f989c650f9f8c59003d15f1e9e2b7e37
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2023-08-24 18:01:18 +0200

    to fix updatemodel

---
+++ Cards/Card.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  Cards
+//
+//  Created by Maryna Bolotska on 24/08/23.
+//
+
+import Foundation
+
+struct Card {
+    
+    
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    
+}
---
+++ Cards/ConcentrationModel.swift
@@ -0,0 +1,19 @@
+//
+//  ConcentrationModel.swift
+//  Cards
+//
+//  Created by Maryna Bolotska on 24/08/23.
+//
+
+import Foundation
+
+
+class ConcentrationModel {
+    var cards: [Card]()
+    
+    func chooseCard(at index: Int) {
+        
+    }
+    
+    
+}
